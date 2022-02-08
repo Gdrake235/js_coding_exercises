c412764fea8179324310b0e09f589ffa83005df7
Gary Drake <gdrake1488@gmail.com>
2022-02-08 11:35:45 +0000

    count sheep wip

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -45,6 +45,14 @@ function getBusNumbers(people) {
 function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
   // Your code here!
+console.log(arr);
+
+ /*  let total = 0;
+  for (let s = 0; i < arr.length; s++) {
+    if (arr[s]) total += 1;
+  }
+  return total; */
+  
 }
 
 function hasMPostCode(person) {
