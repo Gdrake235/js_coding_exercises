3167450ce2c207eb29b7c0f6aa37a3d756fd672a
Gary Drake <gdrake1488@gmail.com>
2022-02-09 09:07:26 +0000

    wip

--- challenges/exercise003.js
+++ challenges/exercise003.js
@@ -25,12 +25,35 @@ function checkIngredients(menu, ingredient) {
   if (menu === undefined) throw new Error("menu is required");
   if (!ingredient) throw new Error("ingredient is required");
   // Your code here!
+  for (let i = 0; i <= ingredient.length; i++) {
+    if (menu !== ingredient[i]) {
+      return false;
+    }
+    else { 
+      return true;
+    }
+    
+  }
 }
 
 function duplicateNumbers(arr1, arr2) {
   if (arr1 === undefined) throw new Error("arr1 is required");
   if (arr2 === undefined) throw new Error("arr2 is required");
   // Your code here!
+    
+    let toMap = {};
+    let result = arr1 + arr2;
+    for (let n = 0; n < arr1.length || arr2.length; n++) {
+
+        if (toMap[arr1[n]]) {
+          result
+          
+        }
+        else {
+          result
+        }
+        return toMap[arr1[n]] = arr1;
+    }
 }
 
 module.exports = {
